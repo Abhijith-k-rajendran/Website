079265fd136aaf9ab278d3266b1b107dd4c35213
Abhijith-k-rajendran <81981595+Abhijith-k-rajendran@users.noreply.github.com>
2023-09-19 22:33:43 +0530

    style changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,13 +12,17 @@ function toggleNavLi() {
         burgerList.style.display = 'none';
         shimmer.style.display = "none";
         spEl1.style.transform = "rotate(0) translate(0,0)";
+        spEl1.style.background = "#000";
         spEl3.style.transform = "rotate(0) translate(0,0)";
+        spEl3.style.background = "#000";
         spEl2.style.opacity = "1";
     } else {
         burgerMenu.classList.add('navActive');
         shimmer.style.display = "flex";
         spEl1.style.transform = "rotate(45deg) translate(0px, 11.2px)";
+        spEl1.style.background = "#fff";
         spEl3.style.transform = "rotate(-45deg) translate(0px, -11.2px)";
+        spEl3.style.background = "#fff";
         spEl2.style.opacity = "0";
         burgerList.style.display = 'flex';
     }
