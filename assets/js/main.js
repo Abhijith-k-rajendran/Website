41e2d8b7b6309c892bbe65dac0baa1ac22ea0273
Abhijith-k-rajendran <81981595+Abhijith-k-rajendran@users.noreply.github.com>
2023-09-19 21:30:14 +0530

    script updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,20 @@
-function toggleNavLi(){
-    const burgerList = document.querySelector('.burgerList');
-    const burgerMenu = document.querySelector('.burgerMenu');
-    const spEl1 = document.getElementById('spOne');
-    const spEl2 = document.getElementById('spTwo');
-    const spEl3 = document.getElementById('spThree');
-    const shimmer = document.querySelector('.shimmer');
+const burgerList = document.querySelector('.burgerList');
+const burgerMenu = document.querySelector('.burgerMenu');
+const spEl1 = document.getElementById('spOne');
+const spEl2 = document.getElementById('spTwo');
+const spEl3 = document.getElementById('spThree');
+const shimmer = document.querySelector('.shimmer');
+const burgerLi = document.querySelector('.burgerList');
 
-    if(burgerMenu.classList.contains('navActive')){
+function toggleNavLi() {
+    if (burgerMenu.classList.contains('navActive')) {
         burgerMenu.classList.remove('navActive');
         burgerList.style.display = 'none';
         shimmer.style.display = "none";
         spEl1.style.transform = "rotate(0) translate(0,0)";
         spEl3.style.transform = "rotate(0) translate(0,0)";
         spEl2.style.opacity = "1";
-    }else{
+    } else {
         burgerMenu.classList.add('navActive');
         shimmer.style.display = "flex";
         spEl1.style.transform = "rotate(45deg) translate(0px, 11.2px)";
@@ -23,10 +24,11 @@ function toggleNavLi(){
     }
 }
 
-const shimmer = document.querySelector('.shimmer');
-const burgerLi = document.querySelector('.burgerList');
-shimmer.addEventListener('click',()=>{
+shimmer.addEventListener('click', () => {
     shimmer.style.display = "none";
     shimmer.style.transition = '0.8s ease-in-out';
     burgerLi.style.display = 'none';
+    spEl1.style.transform = "rotate(0) translate(0,0)";
+    spEl3.style.transform = "rotate(0) translate(0,0)";
+    spEl2.style.opacity = "1";
 });
